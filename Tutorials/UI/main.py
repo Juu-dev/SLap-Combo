24cd6ab6b2dd9cd34451dba259c2163acc0cda26
Juu-dev <thong.pm772002@gmail.com>
2023-11-08 23:39:58 +0700

    Before handing the error of the player continously doing the same action

--- Tutorials/UI/main.py
+++ Tutorials/UI/main.py
@@ -28,8 +28,8 @@ KEY_ACTION_MAPPING = {
 }
 
 # Kích thước mỗi frame trên sprite sheet (đổi lại theo kích thước thực tế của bạn)
-FRAME_WIDTH = 64
-FRAME_HEIGHT = 64
+FRAME_WIDTH = 128
+FRAME_HEIGHT = 128
 ANIMATION_SPEED = 0.15
 
 class Shinobi(pygame.sprite.Sprite):
@@ -37,31 +37,48 @@ class Shinobi(pygame.sprite.Sprite):
         super().__init__()
         self.actions = {}
         self.load_sprites()
-        self.action = 'idle'
+        self.action = 'Idle'
         self.frame_index = 0
         self.image = self.actions[self.action][self.frame_index]
         self.rect = self.image.get_rect()
         self.last_update = pygame.time.get_ticks()
+        self.last_update_action = 0
 
     def load_sprites(self):
         for action in KEY_ACTION_MAPPING.values():
-            sprite_sheet = pygame.image.load(os.path.join('image/shinobi-sprites-pixel-art/Shinobi', f'{action}.png')).convert_alpha()
+            sprite_sheet = pygame.image.load(os.path.join('../image/shinobi-sprites-pixel-art/Shinobi', f'{action}.png')).convert_alpha()
             num_frames = sprite_sheet.get_width() 
-            frames = [sprite_sheet.subsurface(pygame.Rect(i * FRAME_WIDTH, 0, FRAME_WIDTH, FRAME_HEIGHT)) for i in range(num_frames)]
+            frames = [sprite_sheet.subsurface(pygame.Rect(i * FRAME_WIDTH, 0, FRAME_WIDTH, FRAME_HEIGHT)) for i in range(int(num_frames/FRAME_WIDTH))]
             self.actions[action] = frames
 
+    def change_action(self, action):
+        now = pygame.time.get_ticks()
+        if self.action != action and now > self.last_update_action + 1000:
+            self.last_update_action = now
+            self.action = action
+            self.frame_index = 0
+
+    # def update(self):
+    #     now = pygame.time.get_ticks()
+        
+    #     if now - self.last_update > ANIMATION_SPEED * 1000:
+    #         if self.action != 'Idle' and self.frame_index == len(self.actions[self.action])-1:
+    #             print('reset')
+    #             self.frame_index = 0
+    #             self.image = self.actions[self.action][self.frame_index]
+    #             self.change_action('Idle')
+    #         else:
+    #             self.frame_index = (self.frame_index + 1) % len(self.actions[self.action])
+    #             self.image = self.actions[self.action][self.frame_index]
+    #             self.last_update = now
+    
+    # update action 1 time and then stop change to default action is Idle
     def update(self):
         now = pygame.time.get_ticks()
         if now - self.last_update > ANIMATION_SPEED * 1000:
             self.frame_index = (self.frame_index + 1) % len(self.actions[self.action])
             self.image = self.actions[self.action][self.frame_index]
             self.last_update = now
-
-    def change_action(self, action):
-        if self.action != action:
-            self.action = action
-            self.frame_index = 0
-
 class Game:
     def __init__(self):
         self.player = pygame.sprite.GroupSingle() 
@@ -95,7 +112,7 @@ if __name__ == "__main__":
     pygame.display.set_caption("Slap Combo")
 
     # Background and Name
-    background = pygame.image.load().convert_alpha()
+    background = pygame.image.load(BACKGROUND_IMG).convert_alpha()
     text_surface = pygame.font.Font(None, 50).render("My game!", False, 'Green')
     
     shinobi = Shinobi()
@@ -106,14 +123,19 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key in KEY_ACTION_MAPPING:
+                    shinobi.change_action(KEY_ACTION_MAPPING[event.key])
                 
-
         screen.fill((0, 0, 0))
         # draw background with blit and fit it to the screen
         # screen.blit(pygame.transform.scale(background, (1080, 600)), (0, 0))
         screen.blit(text_surface, (500, 100))
+        
+        all_sprites.update()
+        all_sprites.draw(screen)
 
-        game.run()
+        # game.run()
         
         pygame.display.flip()
         clock.tick(FPS)
